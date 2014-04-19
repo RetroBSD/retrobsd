e3e3146c38e6eaea542116f8c67ba570b0878a4e
Matt Jenkins <matt@majenko.co.uk>
2014-04-19 10:01:26 +0100

    Added extra SPI transfer mode IOCTLs - IM

--- sys/include/spi.h
+++ sys/include/spi.h
@@ -15,9 +15,22 @@
 #define SPICTL_SETMODE      _IO ('p', 0)        /* set SPI mode */
 #define SPICTL_SETRATE      _IO ('p', 1)        /* set clock rate, kHz */
 #define SPICTL_SETSELPIN    _IO ('p', 2)        /* set select pin */
+
 #define SPICTL_IO8(n)       _ION('p', 3, n)     /* transfer n*8 bits */
 #define SPICTL_IO16(n)      _ION('p', 4, n)     /* transfer n*16 bits */
 #define SPICTL_IO32(n)      _ION('p', 5, n)     /* transfer n*32 bits */
+
+#define SPICTL_IO8R(n)       _ION('p', 6, n)     /* transfer R n*8 bits */
+#define SPICTL_IO16R(n)      _ION('p', 7, n)     /* transfer R n*16 bits */
+#define SPICTL_IO32R(n)      _ION('p', 8, n)     /* transfer R n*32 bits */
+
+#define SPICTL_IO8W(n)       _ION('p', 9, n)     /* transfer W n*8 bits */
+#define SPICTL_IO16W(n)      _ION('p', 10, n)     /* transfer W n*16 bits */
+#define SPICTL_IO32W(n)      _ION('p', 11, n)     /* transfer W n*32 bits */
+
+#define SPICTL_IO32B(n)      _ION('p', 12, n)     /* transfer BE n*32 bits */
+#define SPICTL_IO32RB(n)      _ION('p', 13, n)     /* transfer RBE n*32 bits */
+#define SPICTL_IO32WB(n)      _ION('p', 14, n)     /* transfer WBE n*32 bits */
 
 #ifdef KERNEL
 #include "conf.h"

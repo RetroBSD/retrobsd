3c025a1e89fe72a6134aa0d0f5a692ec791eb56d
Sergey <serge@vak.ru>
2014-05-05 23:35:40 -0700

    Fixed build errors/warnings for gcc 4.7.2.

--- src/games/trek/main.c
+++ src/games/trek/main.c
@@ -121,7 +121,7 @@ main(argc, argv)
 {
 	long			vect;
 	/* extern FILE		*f_log; */
-	register char		opencode;
+	/* register char	opencode; */
 	int			prio;
 	register int		ac;
 	register char		**av;
@@ -132,7 +132,7 @@ main(argc, argv)
 	av++;
 	time(&vect);
 	srand(vect);
-	opencode = 'w';
+	/* opencode = 'w'; */
 	prio = PRIO;
 	if (ioctl(1, TIOCGETP, &argp) == 0)
 	{
@@ -144,7 +144,7 @@ main(argc, argv)
 		switch (av[0][1])
 		{
 		  case 'a':	/* append to log file */
-			opencode = 'a';
+			/* opencode = 'a'; */
 			break;
 
 		  case 'f':	/* set fast mode */

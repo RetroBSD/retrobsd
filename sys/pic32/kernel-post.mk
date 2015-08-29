c44239cb64a4fc639a68c22116e5eade76919f03
Serge Vakulenko <serge.vakulenko@gmail.com>
2015-08-28 19:32:43 -0700

    Bootloader moved to a separate GitHub repository.

--- sys/pic32/kernel-post.mk
+++ sys/pic32/kernel-post.mk
@@ -12,15 +12,11 @@ SIZE		= $(GCCPREFIX)size
 OBJDUMP		= $(GCCPREFIX)objdump
 OBJCOPY		= $(GCCPREFIX)objcopy
 PROGTOOL        = $(AVRDUDE) -c stk500v2 -p pic32 -b 115200
-BLCFLAGS        = -Os -I. -I$(H) $(DEFS) $(DEPFLAGS)
-BLOBJS          = bl_usb_boot.o bl_usb_device.o bl_usb_function_hid.o bl_devcfg.o
 
 DEFS            += -DCONFIG=$(CONFIG)
 
-all:		.deps sys machine unix.elf $(EXTRA_TARGETS)
+all:		.deps sys machine unix.elf
 		$(SIZE) unix.elf
-
-bootloader:     .deps sys machine bootloader.elf
 
 clean:
 		rm -rf .deps *.o *.elf *.bin *.dis *.map *.srec core \
@@ -45,32 +41,11 @@ unix.elf:       $(KERNOBJ) $(LDSCRIPT)
 		$(OBJCOPY) -O ihex --change-addresses=0x80000000 $@ unix.hex
 		chmod -x $@ unix.bin
 
-bootloader.elf: $(BLOBJS)
-		$(CC) $(LDFLAGS) -T$(BUILDPATH)/cfg/boot.ld -Wl,-Map=usbboot.map $(BLOBJS) -o $@
-		chmod -x $@
-		$(OBJDUMP) -d -S $@ > bootloader.dis
-		$(OBJCOPY) -O ihex --change-addresses=0x80000000 $@ bootloader.hex
-
-bl_usb_boot.o:  $(BUILDPATH)/usb_boot.c
-		$(CC) $(BLCFLAGS) -o $@ -c $<
-
-bl_usb_device.o: $(BUILDPATH)/usb_device.c
-		$(CC) $(BLCFLAGS) -o $@ -c $<
-
-bl_usb_function_hid.o: $(BUILDPATH)/usb_function_hid.c
-		$(CC) $(BLCFLAGS) -o $@ -c $<
-
-bl_devcfg.o:    devcfg.c
-		$(CC) $(BLCFLAGS) -o $@ -c $<
-
 load:           unix.hex
 		pic32prog $(BLREBOOT) unix.hex
 
 loadmax:        unix.hex
 		$(PROGTOOL) -U flash:w:unix.hex:i
-
-loadboot:       bootloader.hex
-		pic32prog $(BLREBOOT) bootloader.hex
 
 vers.o:		$(BUILDPATH)/newvers.sh $(H)/*.h $(M)/*.[ch] $(S)/*.c
 		sh $(BUILDPATH)/newvers.sh > vers.c

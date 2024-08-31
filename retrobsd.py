c743712df84bba05787d03912799c0dfdbf1c9a9
brent hartshorn <brenthartshorn@proton.me>
2024-08-31 07:12:50 -0700

    riscv build script

---
+++ retrobsd.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import os, sys, subprocess
+
+
+def c2o(file, out='/tmp/c2o.o', includes=None, defines=None, opt='-O0', bits=64 ):
+	if 'fedora' in os.uname().nodename:
+		cmd = ['riscv64-linux-gnu-gcc']
+	else:
+		cmd = ['riscv64-unknown-elf-gcc']
+
+	if bits==32:
+		cmd.append('-march=rv32i')
+		cmd.append('-mabi=ilp32')
+
+	cmd += [
+		'-c', '-mcmodel=medany', '-fomit-frame-pointer', '-ffunction-sections',
+		'-ffreestanding', '-nostdlib', '-nostartfiles', '-nodefaultlibs', 
+		opt, '-g', '-o', out, file
+	]
+	if includes:
+		for inc in includes:
+			if not inc.startswith('-I'): inc = '-I'+inc
+			cmd.append(inc)
+	if defines:
+		for d in defines:
+			if not d.startswith('-D'): d = '-D'+d
+			cmd.append(d)
+	
+	print(cmd)
+	subprocess.check_call(cmd)
+	return out
+
+
+def mkkernel():
+	defines = ['KERNEL']
+	includes = [
+		'./include'
+	]
+	obs = []
+	for name in os.listdir('./sys/kernel/'):
+		assert name.endswith('.c')
+		print(name)
+		o = c2o(
+			os.path.join('./sys/kernel', name), 
+			out = '/tmp/%s.o' % name,
+			includes=includes, defines=defines, bits=32)
+		obs.append(o)
+
+	print(obs)
+
+
+if __name__=='__main__':
+	mkkernel()

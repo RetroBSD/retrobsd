76feb0d82f50879933d566fc2fecc86d15728a6c
brent hartshorn <brenthartshorn@proton.me>
2024-08-31 09:20:38 -0700

    struct devspec
    fixes error:
    ```
    ['riscv64-unknown-elf-gcc', '-march=rv32i', '-mabi=ilp32', '-c', '-mcmodel=medany', '-fomit-frame-pointer', '-ffunction-sections', '-ffreestanding', '-nostdlib', '-nostartfiles', '-nodefaultlibs', '-O0', '-g', '-o', '/tmp/subr_log.c.o', './sys/kernel/subr_log.c', '-I./include', '-DKERNEL']
    ./sys/kernel/subr_log.c:35:22: error: array type has incomplete element type 'struct devspec'
       35 | const struct devspec logdevs[] = {
    
    ```

--- sys/pic32/riscv.h
+++ sys/pic32/riscv.h
@@ -2,3 +2,8 @@
 
 #define ST_RP           0x08000000      /* Enable reduced power mode */
 #define ST_UM           0x00000010      /* User mode */
+
+struct devspec {
+	unsigned _id;
+	const char *_name;
+};

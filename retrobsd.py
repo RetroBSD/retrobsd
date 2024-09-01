a6990213306fb92fb4e2fe95c1bee2fd516413b3
brent hartshorn <brenthartshorn@proton.me>
2024-09-01 08:38:51 -0700

    minimal main
    riscv64-unknown-elf-ld: /home/rap/cbacktick/retrobsd/./sys/kernel/kern_sysctl.c:616: undefined reference to `file'
    riscv64-unknown-elf-ld: /home/rap/cbacktick/retrobsd/./sys/kernel/kern_sysctl.c:632: undefined reference to `file'
    riscv64-unknown-elf-ld: /home/rap/cbacktick/retrobsd/./sys/kernel/kern_sysctl.c:640: undefined reference to `copyout'
    riscv64-unknown-elf-ld: /home/rap/cbacktick/retrobsd/./sys/kernel/kern_sysctl.c:641: undefined reference to `copyout'
    riscv64-unknown-elf-ld: /home/rap/cbacktick/retrobsd/./sys/kernel/kern_sysctl.c:632: undefined reference to `file'
    riscv64-unknown-elf-ld: /tmp/kern_sysctl.c.o: in function `sysctl_inode':
    /home/rap/cbacktick/retrobsd/./sys/kernel/kern_sysctl.c:687: undefined reference to `inode'
    riscv64-unknown-elf-ld: /home/rap/cbacktick/retrobsd/./sys/kernel/kern_sysctl.c:687: undefined reference to `inode'
    riscv64-unknown-elf-ld: /home/rap/cbacktick/retrobsd/./sys/kernel/kern_sysctl.c:698: undefined reference to `inode'
    riscv64-unknown-elf-ld: /home/rap/cbacktick/retrobsd/./sys/kernel/kern_sysctl.c:706: undefined reference to `copyout'
    riscv64-unknown-elf-ld: /home/rap/cbacktick/retrobsd/./sys/kernel/kern_sysctl.c:707: undefined reference to `copyout'
    riscv64-unknown-elf-ld: /home/rap/cbacktick/retrobsd/./sys/kernel/kern_sysctl.c:698: undefined reference to `inode'
    riscv64-unknown-elf-ld: /tmp/kern_sysctl.c.o: in function `fill_from_u':
    /home/rap/cbacktick/retrobsd/./sys/kernel/kern_sysctl.c:753: undefined reference to `swapdev'
    riscv64-unknown-elf-ld: /home/rap/cbacktick/retrobsd/./sys/kernel/kern_sysctl.c:757: undefined reference to `swapdev'
    riscv64-unknown-elf-ld: /tmp/kern_sysctl.c.o: in function `sysctl_doproc':
    /home/rap/cbacktick/retrobsd/./sys/kernel/kern_sysctl.c:881: undefined reference to `copyout'
    riscv64-unknown-elf-ld: /home/rap/cbacktick/retrobsd/./sys/kernel/kern_sysctl.c:885: undefined reference to `copyout'
    riscv64-unknown-elf-ld: /tmp/_riscv_devsw.o:(.rodata+0xdc): undefined reference to `mmrw'
    riscv64-unknown-elf-ld: /tmp/_riscv_devsw.o:(.rodata+0xe0): undefined reference to `mmrw'
    riscv64-unknown-elf-ld: /tmp/_riscv_swap.o: in function `swopen':
    /home/rap/cbacktick/retrobsd/./sys/pic32/swap.c:37: undefined reference to `swapdev'
    riscv64-unknown-elf-ld: /home/rap/cbacktick/retrobsd/./sys/pic32/swap.c:37: undefined reference to `swapdev'
    riscv64-unknown-elf-ld: /tmp/_riscv_swap.o: in function `swclose':
    /home/rap/cbacktick/retrobsd/./sys/pic32/swap.c:49: undefined reference to `swapdev'
    riscv64-unknown-elf-ld: /home/rap/cbacktick/retrobsd/./sys/pic32/swap.c:49: undefined reference to `swapdev'
    riscv64-unknown-elf-ld: /tmp/_riscv_swap.o: in function `swcioctl':
    /home/rap/cbacktick/retrobsd/./sys/pic32/swap.c:193: undefined reference to `swapmap'
    riscv64-unknown-elf-ld: /home/rap/cbacktick/retrobsd/./sys/pic32/swap.c:195: undefined reference to `swapmap'
    riscv64-unknown-elf-ld: /tmp/_riscv_swap.o: in function `swstrategy':
    /home/rap/cbacktick/retrobsd/./sys/pic32/swap.c:224: undefined reference to `swapdev'
    riscv64-unknown-elf-ld: /home/rap/cbacktick/retrobsd/./sys/pic32/swap.c:225: undefined reference to `swapdev'
    Traceback (most recent call last):
      File "/home/rap/cbacktick/retrobsd/retrobsd.py", line 117, in <module>
        mkkernel()

--- retrobsd.py
+++ retrobsd.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python3
 import os, sys, subprocess
 
+RISCV_MACHINE_MIN = '''
+#include "sys/param.h"
+#include "sys/user.h"
+#include "sys/proc.h"
+int	cmask = CMASK;
+
+int securelevel = -1;
+
+struct user u = {};
+void _start(){
+	struct proc *p;
+	p = &proc[0];
+	//p->p_addr = *ka6;
+	p->p_stat = SRUN;
+	p->p_flag |= SLOAD|SSYS;
+	p->p_nice = NZERO;
+
+	u.u_procp = p;			/* init user structure */
+	//u.u_ap = u.u_arg;
+	u.u_cmask = cmask;
+	u.u_lastfile = -1;
+
+#ifdef INET
+	if (netoff = netinit())
+		printf("netinit failed\n");
+	else
+		{
+		NETSETHZ();
+		NETSTART();
+		}
+#endif
+
+}
+'''
+
 
 def c2o(file, out='/tmp/c2o.o', includes=None, defines=None, opt='-O0', bits=64 ):
 	if 'fedora' in os.uname().nodename:
@@ -9,7 +44,7 @@ def c2o(file, out='/tmp/c2o.o', includes=None, defines=None, opt='-O0', bits=64 
 		cmd = ['riscv64-unknown-elf-gcc']
 
 	if bits==32:
-		cmd.append('-march=rv32i')
+		cmd.append('-march=rv32imac')
 		cmd.append('-mabi=ilp32')
 
 	cmd += [
@@ -32,11 +67,24 @@ def c2o(file, out='/tmp/c2o.o', includes=None, defines=None, opt='-O0', bits=64 
 
 
 def mkkernel():
-	defines = ['KERNEL']
+	defines = [
+		'KERNEL', 
+		'CONS_MAJOR=UART_MAJOR',
+	]
 	includes = [
 		'./include'
 	]
 	obs = []
+
+	rtmp = '/tmp/_riscv_.c'
+	open(rtmp,'w').write(RISCV_MACHINE_MIN)
+	o = c2o(
+		rtmp, 
+		out = '/tmp/_riscv_.o',
+		includes=includes, defines=defines, bits=32)
+	obs.append(o)
+
+
 	for name in os.listdir('./sys/kernel/'):
 		assert name.endswith('.c')
 		print(name)
@@ -47,12 +95,14 @@ def mkkernel():
 		obs.append(o)
 
 	macho = []
-	for name in 'machdep'.split():
+	#for name in 'machdep'.split():
+	for name in 'devsw cons swap'.split():
 		o = c2o(
 			'./sys/pic32/%s.c' % name, 
 			out = '/tmp/_riscv_%s.o' % name,
 			includes=includes, defines=defines, bits=32)
 		macho.append(o)
+
 
 	if 'fedora' in os.uname().nodename:
 		cmd = ['riscv64-linux-gnu-ld']

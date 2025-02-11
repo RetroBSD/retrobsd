58e05198e469f2e6bd640aa4c3eb7999bbc23a94
Serge <serge.vakulenko@gmail.com>
2025-02-10 18:56:55 -0800

    Create top level clang-config.mk: find clang version.

---
+++ clang-config.mk
@@ -0,0 +1,55 @@
+# Clang compiler on Linux
+# ~~~~~~~~~~~~~~~~~~~~~~~
+# Install by:
+#   sudo apt install clang-18
+# Newer versions are also OK.
+ifeq ($(LLVMBIN),)
+    LLVMBIN = $(dir $(wildcard /usr/lib/llvm-18/bin/clang))
+endif
+ifeq ($(LLVMBIN),)
+    LLVMBIN = $(dir $(wildcard /usr/lib/llvm-17/bin/clang))
+endif
+ifeq ($(LLVMBIN),)
+    # Default on Ubuntu 24.04
+    LLVMBIN = $(dir $(wildcard /usr/lib/llvm-16/bin/clang))
+endif
+ifeq ($(LLVMBIN),)
+    LLVMBIN = $(dir $(wildcard /usr/lib/llvm-15/bin/clang))
+endif
+ifeq ($(LLVMBIN),)
+    # Default on Debian 12 amd Ubuntu 22.04
+    LLVMBIN = $(dir $(wildcard /usr/lib/llvm-14/bin/clang))
+endif
+
+# Clang compiler on MacOS
+# ~~~~~~~~~~~~~~~~~~~~~~~
+# Install by:
+#   brew install clang@18
+# Newer versions are also OK.
+ifeq ($(LLVMBIN),)
+    LLVMBIN = $(dir $(wildcard /usr/local/Cellar/llvm@18/18.*/bin/clang-18))
+endif
+ifeq ($(LLVMBIN),)
+    LLVMBIN = $(dir $(wildcard /usr/local/Cellar/llvm@17/17.*/bin/clang-17))
+endif
+ifeq ($(LLVMBIN),)
+    LLVMBIN = $(dir $(wildcard /usr/local/Cellar/llvm@16/16.*/bin/clang-16))
+endif
+ifeq ($(LLVMBIN),)
+    LLVMBIN = $(dir $(wildcard /usr/local/Cellar/llvm@15/15.*/bin/clang-15))
+endif
+ifeq ($(LLVMBIN),)
+    LLVMBIN = $(dir $(wildcard /usr/local/Cellar/llvm@14/14.*/bin/clang-14))
+endif
+
+# Default Clang compiler
+# ~~~~~~~~~~~~~~~~~~~~~~
+ifeq ($(LLVMBIN),)
+    # Note: native clang on MacOS has no support for mips32.
+    ifneq ($(shell uname -s),Darwin)
+        LLVMBIN = $(dir $(wildcard /usr/bin/clang))
+    endif
+endif
+ifeq ($(LLVMBIN),)
+    $(error Unable to find any CLANG toolchain!)
+endif
--- target.mk
+++ target.mk
@@ -4,36 +4,7 @@ RELEASE     = 0.0
 BUILD       = $(shell git rev-list HEAD --count)
 VERSION     = $(RELEASE)-$(BUILD)
 
-# Clang compiler on Linux
-# ~~~~~~~~~~~~~~~~~~~~~~~
-# Install by:
-#   sudo apt install clang-18
-# Newer versions are also OK.
-ifeq ($(LLVMBIN),)
-    LLVMBIN = $(dir $(wildcard /usr/lib/llvm-18/bin/clang))
-endif
-ifeq ($(LLVMBIN),)
-    LLVMBIN = $(dir $(wildcard /usr/lib/llvm-16/bin/clang))
-endif
-ifeq ($(LLVMBIN),)
-    LLVMBIN = $(dir $(wildcard /usr/lib/llvm-15/bin/clang))
-endif
-
-# Clang compiler on MacOS
-# ~~~~~~~~~~~~~~~~~~~~~~~
-# Install by:
-#   brew install clang@18
-# Newer versions are also OK.
-ifeq ($(LLVMBIN),)
-    LLVMBIN = $(dir $(wildcard /usr/local/Cellar/llvm@18/18.*/bin/clang-18))
-endif
-
-ifeq ($(LLVMBIN),)
-    LLVMBIN = $(dir $(wildcard /usr/bin/clang))
-endif
-ifeq ($(LLVMBIN),)
-    $(error Unable to find any CLANG toolchain!)
-endif
+include $(TOPSRC)/clang-config.mk
 
 # TODO: remove -Wno-deprecated-non-prototype -Wno-implicit-int
 CC		= $(LLVMBIN)clang -target mipsel -mcpu=mips32r2 -mabi=o32 -msoft-float \
